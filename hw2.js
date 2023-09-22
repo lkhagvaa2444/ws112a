4d9378f70d2067cc37bdbd8df6e212a7d3385ad7
Lkhagvasuren <lkhagvaa2444@gmail.com>
2023-09-22 11:26:02 +0800

    commit

--- hw2.js
+++ hw2.js
@@ -1,27 +1,15 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 const books = new Map();
-books.set("1", {
-  id: "1",
-  title: "The Hound of the Baskervilles",
-  author: "Conan Doyle, Arthur",
-});
-books.set("2", {
-  id: "2",
-  title: "The Old Man",
-  author: "Lee Ear",
-});
 
 const classrooms = new Map();
 classrooms.set("e320", {
-    roomNumber: "e320",
+    id: "e320",
     class: "Multimedia Classroom",
-    teacher: "Teacher",
 });
 classrooms.set("e319", {
-    roomNumber: "e319",
+    id: "e319",
     class: "Embedded Systems Lab",
-    teacher: "Teacher",
 });
 
 const router = new Router();
@@ -29,25 +17,36 @@ router
   .get("/", (context) => {
     context.response.body = "Hello world!";
   })
-  .get("/book", (context) => {
-    context.response.body = Array.from(books.values());
-  })
-  .get("/book/:id", (context) => {
-    if (context.params && context.params.id && books.has(context.params.id)) {
-      context.response.body = books.get(context.params.id);
-    }
-  })
-  .get("/room", (context) => {
-    context.response.body = Array.from(classrooms.values());
-  })
-  .get("/room/:roomNumber", (context) => {
-    if (context.params && context.params.roomNumber && classrooms.has(context.params.roomNumber)) {
-      const room = classrooms.get(context.params.roomNumber);
-      context.response.body = `Room ${room.roomNumber} is ${room.class}`;
-    } else {
-      context.response.body = "Room not found.";
-    }
-  });
+  .get("/nqu", (context) => {
+    context.response.body = `
+<html>
+    <body>
+        <a href="https://www.nqu.edu.tw/">National Quemoy University</a>
+    </body>
+</html>`
+})
+.get("/nqu/csie", (context) => {
+    context.response.body = `
+<html>
+    <body>
+        <a href="https://csie.nqu.edu.tw/">Computer Science And information Engineering Department</a>
+    </body>
+</html>`
+})
+.get("/to/nqu", (context) => {
+    context.response.redirect('https://www.nqu.edu.tw/')
+})
+.get("/to/nqu/csie", (context) => {
+    context.response.redirect('https://csie.nqu.edu.tw/')
+})
+.get("/room", (context) => {
+  context.response.body = Array.from(classrooms.values());
+})
+.get("/room/:id", (context) => {
+  if (context.params && context.params.id && classrooms.has(context.params.id)) {
+    context.response.body = classrooms.get(context.params.id);
+}
+});
 
 const app = new Application();
 app.use(router.routes());
